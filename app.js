1d60e51edd40d7baa6b8214fe7653e163dbc540d
kj6560 <kj6560@gmail.com>
2023-12-15 22:20:13 +0530

    changes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use('/api/v1', api);
 app.use('/',web);
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
